f296bb3e09c82267dcf3061e5edd681ef04a803b
baya laadjel <bata_cher@hotmail.fr>
2023-11-04 15:17:18 +0100

    ajout dossier styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Home.css'
+import '../styles/Home.css'
 import BankLogo from '../assets/argentBankLogo.png'
 import IconChat from '../assets/icon-chat.png'
 import IconMoney from '../assets/icon-money.png'
