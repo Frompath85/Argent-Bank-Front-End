c942a1718bf793d65e0c5732f16f0dbd56f01d86
baya laadjel <bata_cher@hotmail.fr>
2023-11-05 21:05:23 +0100

    ajout routes

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,26 @@
-import './app.css'
+import './styles/app.css'
 import Home from './pages/Home'
+import SignIn from './pages/SignIn';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+
 export function App() {
 
   return (
     <div>
-     <Home></Home>
-     
+      <Header></Header>
+      <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/Login" element ={<SignIn />} />
+                  {/*<Route path="/Logement/:id" element ={<Logement />} />
+                <Route path="*" element ={<NotFoundPage />} /> */}
+            </Routes>
+
+        </Router>
+      <Footer></Footer>
     </div>
   )
 }
