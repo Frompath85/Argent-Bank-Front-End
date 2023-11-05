230f16fa1e4467a07ae398d99be93b0ed18639be
baya laadjel <bata_cher@hotmail.fr>
2023-11-05 22:57:50 +0100

    ajout condition OnUserPage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,8 @@ import BankLogo from '../assets/argentBankLogo.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faCircleUser, faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
 
-export default function Header() {
+export default function Header(props) {
+  // console.log(props.OnUserPage)
   return (
     <nav className="main-nav"> 
       <a className="main-nav-logo" href="./index.html">
@@ -15,13 +16,26 @@ export default function Header() {
         />
         <h1 className="sr-only">Argent Bank</h1>
       </a>
-     
+     {
+     props.OnUserPage =='false' ? 
       <div>
-        <a className="main-nav-item" href="./sign-in.html">
-        <FontAwesomeIcon icon={faCircleUser} />
-        <span>   Sign In</span>
-        </a>
+          <a className="main-nav-item" href="./sign-in.html">
+          <FontAwesomeIcon icon={faCircleUser} />
+          <span>   Sign In</span>
+          </a>
+      </div> 
+      :  
+      <div>
+          <a className="main-nav-item" href="./sign-in.html">
+          <FontAwesomeIcon icon={faCircleUser} />
+          <span>   UserName</span>
+          </a>
+          <a className="main-nav-item" href="./sign-in.html">
+          <FontAwesomeIcon icon={faRightFromBracket} />
+          <span>   Sign Out</span>
+          </a>
       </div>
+     }
     </nav>
   )
 }
