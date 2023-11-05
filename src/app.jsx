be5f83e6bbe1bc8b79c40cbbe75bb29460b66684
baya laadjel <bata_cher@hotmail.fr>
2023-11-05 22:57:19 +0100

    ajout route /user

--- src/app.jsx
+++ src/app.jsx
@@ -2,21 +2,21 @@ import './styles/app.css'
 import Home from './pages/Home'
 import SignIn from './pages/SignIn';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Header from './components/Header';
 import Footer from './components/Footer';
-
+import User from './pages/User';
 
 export function App() {
 
   return (
     <div>
-      <Header></Header>
       <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/Login" element ={<SignIn />} />
-                  {/*<Route path="/Logement/:id" element ={<Logement />} />
-                <Route path="*" element ={<NotFoundPage />} /> */}
+                <Route path="/User" element ={<User />} />
+
+                {/*<<Route path="/Logement/:id" element ={<Logement />} />
+                Route path="*" element ={<NotFoundPage />} /> */}
             </Routes>
 
         </Router>
