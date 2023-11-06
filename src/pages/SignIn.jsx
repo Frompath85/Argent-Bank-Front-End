049cf7f20c37e041bd019f5c7ddd39ba1ed400a0
baya laadjel <bata_cher@hotmail.fr>
2023-11-06 22:34:41 +0100

    ajout composant SignInForm

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,31 +1,14 @@
 import React from 'react'
 import '../styles/Home.css'
 import Header from '../components/Header'
+import SignInForm from '../components/SignInForm'
 
 export default function SignIn() {
   return (
     <>
-    <Header />
+    <Header OnUserPage = 'false'/>
     <main class="main bg-dark">
-    <section class="sign-in-content">
-      <i class="fa fa-user-circle sign-in-icon"></i>
-      <h1>Sign In</h1>
-      <form>
-        <div class="input-wrapper">
-          <label for="username">Username</label>
-          <input type="text" id="username" />
-        </div>
-        <div class="input-wrapper">
-          <label for="password">Password</label>
-          <input type="password" id="password" />
-        </div>
-        <div class="input-remember">
-          <input type="checkbox" id="remember-me" />
-          <label for="remember-me">Remember me </label >
-        </div>
-        <button class="sign-in-button">Sign In</button> 
-      </form>
-    </section>
+    <SignInForm />
   </main>
   </>
   )
