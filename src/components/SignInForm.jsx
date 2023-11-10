34457ba49ee63f29e0917dfdd1b3bdfa6c61e203
baya laadjel <bata_cher@hotmail.fr>
2023-11-10 09:28:48 +0100

    import GetToken as a function

--- src/components/SignInForm.jsx
+++ src/components/SignInForm.jsx
@@ -3,53 +3,36 @@ import {useRef, useState} from 'react'
 import '../styles/Home.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faSignIn } from '@fortawesome/free-solid-svg-icons'
+import { useNavigate } from 'react-router-dom'
+import { GetToken } from '../services/services'
 
+export default  function  SignInForm() {
 
-export default function SignInForm() {
     const emailInputRef = useRef();
     const passwordInputRef = useRef();
+    const navigate = useNavigate() 
 
     const [TokendData, setData]= useState();
-     console.log(TokendData)
+    // console.log(TokendData)
      
-    const SubmitHundler = (event) =>{
+    const SubmitHundler = async (event) =>{
 
       event.preventDefault();
       const entredEmail = emailInputRef.current.value
       const entredPassword = passwordInputRef.current.value
       console.log(entredEmail, entredPassword);
 
-      const url = 'http://localhost:3001/api/v1/user/login';
+      const token =  await GetToken(entredEmail,entredPassword)
+      // console.log(token)
+      setData(token)
 
-      // try{
-          fetch(url, {
-              method : 'POST',
-              body :JSON.stringify({
-                  email : entredEmail,
-                  password : entredPassword,
-              }),
-              headers:{
-                  'Content-Type':"application/json",
-              }
-          })
-          .then((response)=> response.json())
-          .then((data)=>{
-            if(data.status == 200)
-                 setData(data)
-            else alert(data.message)
-          })// message d'erreur s'affiche dans data
-           .catch((err)=>console.log(err))
-        // }  
-        // catch( error) {// lorsque le serveur ne repond pas 
-        //   console.log(error)
-        // }
-}
+    }
 
   return (
     <section class="sign-in-content">
     <FontAwesomeIcon icon={faSignIn} />
     <h1>Sign In</h1>
-    <form onSubmit={SubmitHundler}>
+    <form >
       <div class="input-wrapper">
         <label for="username">Username</label>
         <input type="text" id="username" ref={emailInputRef}/>
@@ -62,7 +45,9 @@ export default function SignInForm() {
         <input type="checkbox" id="remember-me" />
         <label for="remember-me">Remember me </label >
       </div>
-      <button class="sign-in-button" type='submit'>Sign In</button> 
+  
+      <button class="sign-in-button"  onClick={ TokendData? navigate('/User'): SubmitHundler}>Sign In</button> 
+    
     </form>
   </section>
   )
