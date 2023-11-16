795a358ac022ad6eab92b24d305cdead616f6355
baya laadjel <bata_cher@hotmail.fr>
2023-11-16 15:55:19 +0100

    add edit section

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -5,14 +5,19 @@ import { GetUserData } from '../services/services'
 import {useDispatch, useSelector} from 'react-redux'
 import { setEmail, setFirstName, setLastName } from '../features/dataReducer' 
 import { Navigate } from 'react-router-dom'
+import { useState, useEffect } from "react";
 
 
 export default   function User() {
+
   const dispatch = useDispatch(); 
+
+  const[NewFirstName, setNewFirstName]= useState("")
+  const[NewLastName, setNewLastName]= useState("")
 
   const token = useSelector(state => state.data.token)
   console.log(token)
-  if(token == "") return <Navigate to="/login" />
+  // if(token == "") return <Navigate to="/login" />
 
   const UserData = GetUserData(token);
   UserData.then(user => {
@@ -22,21 +27,46 @@ export default   function User() {
 });
 
 const email = useSelector(state => state.data.email)
-console.log(email)
 const firstname = useSelector(state => state.data.firstName)
-console.log(firstname)
 const lastname = useSelector(state => state.data.lastName)
-console.log(lastname)
+
+const EditProfil = ()=>{
+  console.log("Edit")
+  document.getElementById("edit-section").style.display = "block";
+  document.querySelector(".edit-button").style.display = "none";
+  document.querySelector(".first-last-name").style.display = "none";
+}
+const SaveProfil = ()=>{
+  console.log("save")
+}
+const CancelProfil = ()=>{
+  console.log("Cancel edit ")
+}
+
 
   return (
     <>
     <Header OnUserPage = 'true' UserName = {firstname}/>
-    {/* //Tony first name */}
     <main class="main bg-dark-user">
     <div class="header">
-      <h1>Welcome back<br /> {firstname} {lastname} </h1>
-      <button class="edit-button">Edit Name</button>
+      <h1>Welcome back<br /> <span className='first-last-name'> hani {firstname} {lastname} </span></h1>
+      <button class="edit-button" onClick={EditProfil}>Edit Name</button>
     </div>
+    {/* section modification données Profil */}
+    <div id='edit-section'>
+      <form id='edit-form'>
+        <div className='input-section'>
+           <input placeholder = {firstname} className='edit-profil-input' type="text" id='firstName' onChange={e => setNewFirstName(e.target.value)}/> 
+           <input placeholder = {lastname} className='edit-profil-input' type="text" id='lastName' onChange={e => setNewLastName(e.target.value)}/>
+        </div>
+        <div className='button-section'>
+          <button className='edit-profil-button' onClick={SaveProfil}>Save</button>
+          <button className='edit-profil-button' onClick={CancelProfil}>Cancel</button>
+        </div>
+      </form>
+
+    </div>
+
     <h2 class="sr-only">Accounts</h2>
     <section class="account">
       <div class="account-content-wrapper">
