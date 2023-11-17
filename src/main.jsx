1e51eafe6a1e119206882686a946b1608aa02e10
baya laadjel <bata_cher@hotmail.fr>
2023-11-17 15:54:42 +0100

    delete comment

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
 import { render } from 'preact'
 import { App } from './app.jsx'
 import './styles/index.css'
-// import {store } from './store.js'
-// import { Provider } from 'react-redux'
-{/* <Provider store={store}></Provider> */}
+ 
 render(
     
         <App />, document.getElementById('app')
