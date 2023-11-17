a3acbdcca2e9b2f93345f808b206e138f1a3a5e3
baya laadjel <bata_cher@hotmail.fr>
2023-11-17 15:55:52 +0100

    ajout de test de connexion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,12 @@ import BankLogo from '../assets/argentBankLogo.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faCircleUser, faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom'
+import { useSelector} from 'react-redux'
 
-
-export default function Header(props) {
+export default function Header() {
+  // je teste si je suis connecté ou pas
+  const token = useSelector(state => state.data.token)
+  const firstname = useSelector(state => state.data.firstName)
 
   return (
     <nav className="main-nav"> 
@@ -18,25 +21,24 @@ export default function Header(props) {
         />
         <h1 className="sr-only">Argent Bank</h1>
     </Link>
-     {
-     props.OnUserPage =='false' ?
-      <div>
-        <Link className="main-nav-item" to={"/Login"}>
-          <FontAwesomeIcon icon={faCircleUser} />
-          <span>   Sign In</span>
-        </Link>
-      </div> 
-      :  
-      <div>
-        <Link className="main-nav-item" to="/User">
-          <FontAwesomeIcon icon={faCircleUser} />
-          <span> {props.UserName}</span>
-        </Link>  
-        <Link className="main-nav-item" to="/LogOut">  
-          <FontAwesomeIcon icon={faRightFromBracket} />
-          <span> Sign Out</span>
-        </Link>  
-      </div>
+     { token ?  
+        <div>
+          <Link className="main-nav-item" to="/Profile">
+            <FontAwesomeIcon icon={faCircleUser} />
+            <span> {firstname} </span>
+          </Link>  
+          <Link className="main-nav-item" to="/LogOut">  
+            <FontAwesomeIcon icon={faRightFromBracket} />
+            <span> Sign Out</span>
+          </Link>  
+        </div>
+      :
+        <div>
+          <Link className="main-nav-item" to={"/Login"}>
+            <FontAwesomeIcon icon={faCircleUser} />
+            <span>   Sign In</span>
+          </Link>
+        </div> 
      }
     </nav>
   )
